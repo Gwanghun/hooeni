d70d9617555da8ed08e59eed9018d3c3028523f3
hooeni <hooeni@gmail.com>
2019-04-24 10:01:55 +0900

    수정

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <? # META START ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <? # meta ?>
+
+  <title> Weekly </title>
+
+  <link href="./assets/css/reset.css" rel="stylesheet" title="Reset">
+</head>
+<body>
